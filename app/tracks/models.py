f80de3a47b3d36951a35280175b9473158fdbb60
Nick <nick.smith.tsa@gmail.com>
2021-02-15 22:43:45 +0000

    added create tracks mutation

--- app/tracks/models.py
+++ app/tracks/models.py
@@ -7,6 +7,7 @@ from django.db import models
 class Track(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField(blank=True)
+    url = models.URLField()
     created_at = models.DateTimeField(auto_now_add=True)
 
 # each track should have a title, created_at and optional description
