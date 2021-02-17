eee5e6af5ac57ebf5f78f83261116454fcb4cf98
Nick <nick.smith.tsa@gmail.com>
2021-02-17 22:26:34 +0000

    create use mutation

---
+++ app/users/schema.py
@@ -0,0 +1,52 @@
+from django.contrib.auth import get_user_model
+import graphene
+from graphene_django import DjangoObjectType # to create class
+
+class UserType(DjangoObjectType):
+    class Meta:
+        model = get_user_model() # how we access user model
+        # could use this to specify specific fields to use: only_fields = ('id', 'email', 'password', 'username')
+
+#mutation for creating new user
+class CreateUser(graphene.Mutation):
+    user = graphene.Field(UserType)
+
+    class Arguments:
+        username = graphene.String(required=True)
+        password = graphene.String(required=True)
+        email = graphene.String(required=True)
+
+    def mutate(self, info, username, password, email):
+        user = get_user_model()(
+            username=username,
+            email=email
+        )
+        user.set_password(password)
+        user.save()
+        return CreateUser(user=user)
+
+class Mutation(graphene.ObjectType):
+    create_user = CreateUser.Field() # error here?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
