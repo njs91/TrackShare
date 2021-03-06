f87038090034538e27ae753bc907e76f1c527275
Nick <nick.smith.tsa@gmail.com>
2021-03-06 22:11:32 +0000

    register form

--- react-tracks-client/src/components/Auth/index.js
+++ react-tracks-client/src/components/Auth/index.js
@@ -1,7 +1,8 @@
 import React from "react";
-
+import Login from './Login'
+import Register from './Register'
 import withRoot from "../../withRoot";
 
 export default withRoot(() => {
-  return <div>Auth</div>;
+  return <Register/>;
 });
