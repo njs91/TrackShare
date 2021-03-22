a2873a142210059f4624033b712e70d3fe11831d
Nick <nick.smith.tsa@gmail.com>
2021-03-22 22:14:45 +0000

    update track & part of delete track

--- react-tracks-client/src/Root.js
+++ react-tracks-client/src/Root.js
@@ -9,22 +9,24 @@ import Header from './components/Shared/Header'
 import Loading from './components/Shared/Loading'
 import Error from './components/Shared/Error'
 
+export const UserContext = React.createContext()
+
 const Root = () => (
   <Query query={ME_QUERY}>
     {({ data, loading, error }) => {
       if (loading) return <Loading/>;
       if (error) return <Error error={error}/>;
-      const user = data.me;
+      const currentUser = data.me;
 
       return (
       <Router>
-         <>
-            <Header user={user}/>
+         <UserContext.Provider value={currentUser}>
+            <Header user={currentUser}/>
             <Switch>
                 <Route exact path="/" component={App} />
                 <Route path="/profile/:id" component={Profile} />
             </Switch>
-         </>
+         </UserContext.Provider>
       </Router>
       );
     }}
